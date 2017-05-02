e11576f55529a3e267ea8a9e74fb950cadbe58a6
Game110AM <Game110AM@605-110-STU19>
2017-05-02 08:04:27 -0600

    commit

---
+++ FavoriteFoods/FavoriteFoods/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+		string food[8];
+
+		food[0] = "Burger";
+		food[1] = "Pizza";
+		food[2] = "Fruit";
+		food[3] = "Pasta";
+		food[4] = "Tacos";
+		food[5] = "Ham";
+		food[6] = "Popcorn";
+		food[7] = "Vegetables";
+
+	for (int i = 0; i < 8; i++)
+	cout << food[i] << endl;
+}
